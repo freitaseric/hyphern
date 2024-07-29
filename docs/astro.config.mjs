5d8c1fef339a4a8295df68af526d0392e09ca4d1
Eric Freitas <freitaseric@proton.me>
2024-07-28 22:19:41 -0400

    fix(docs): update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Guides',
-					autogenerate: { directory: 'reference' },
+					autogenerate: { directory: 'guides' },
 				},
 				{
 					label: 'Reference',
